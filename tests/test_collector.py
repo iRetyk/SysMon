d83dc0c0c713c443b6d9344238d33d327b8e0cae
Idan Retyk <idaniretyk@gmail.com>
2026-03-17 01:11:38 +0200

    add test for test_collector

---
+++ tests/test_collector.py
@@ -0,0 +1,11 @@
+from src import get_cpu_usage
+
+
+
+def test_cpu(mocker):
+    mocker.patch("src.collector.psutil.cpu_percent",return_value=[10.0,20.0,30.0,40.0])
+    
+    per_core,total = get_cpu_usage(2)
+    
+    assert per_core == [10.0,20.0,30.0,40.0]
+    assert total == 4
